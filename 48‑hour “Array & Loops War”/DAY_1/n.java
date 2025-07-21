86d357ad6cc6f5e8bc22ed554851d5d8a96e21cb
VIVEK KUMAR <ecstuds@gmail.com>
2025-07-21 12:24:39 +0530

    Add Java programs to print numbers from 0 to n
    Added two Java files under DAY_1 that print numbers from 0 to n using a loop. One file includes a package declaration and comments, while the other is a minimal version.

---
+++ 48‑hour “Array & Loops War”/DAY_1/n.java
@@ -0,0 +1,10 @@
+
+public class n {
+     public static void main(String[] args) {
+        int n =10 ;
+        // Loop from 0 to n
+        for (int i = 0; i <= n; i++) {
+            System.out.println(i);
+        }
+     }
+}
