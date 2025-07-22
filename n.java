c159f616baece426e136cfef8b465cbdbaa41555
VIVEK KUMAR <ecstuds@gmail.com>
2025-07-22 19:41:14 +0530

    Refactor and move Java loop files
    Deleted the old n.java from the loops subdirectory, moved and updated n.java to print numbers from 1 to n instead of 0 to n, and added a new na.java file with similar functionality.

--- n.java
+++ n.java
@@ -1,12 +1,12 @@
-// This is a Java program that prints numbers from 0 to n using a loop.
-package 48_hour_“Array_&_Loops_War”.DAY_1;
-
 public class n {
+    
+
      public static void main(String[] args) {
         int n =10 ;
         // Loop from 0 to n
-        for (int i = 0; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             System.out.println(i);
         }
      }
+
 }
--- na.java
+++ na.java
@@ -1,10 +1,13 @@
-
-public class n {
+public class na {
+  
      public static void main(String[] args) {
         int n =10 ;
         // Loop from 0 to n
-        for (int i = 0; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             System.out.println(i);
         }
-     }
+    
+
+}
+
 }
